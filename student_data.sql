281486612f322043db1ac67ec2aa3adcd567e207
Aman Pandit <amanpandit3027@gmail.com>
2023-03-18 08:00:19 +0530

    Create student_data.sql

---
+++ student_data.sql
@@ -0,0 +1,16 @@
+-- This sql code insert student data such as student_id, first_name, last_name, class_id into the table students.
+
+INSERT INTO students (student_id, first_name, last_name, class_id)
+VALUES (‘20731’, ‘Demo’, ‘Student’, ‘COMPSCI101’),
+	(‘10234’, ‘Riely’,’col’,’SQL101’),
+	(‘75401’, ‘David’, ‘Cris’,’ENG001’),
+  (‘45641’, ‘Denise’, ‘Tucker’, ‘COMPSCI101’),
+  (‘04291’, ‘Joel’, ‘Rhodes’, ‘ENG101’),
+  (‘50139’, ‘Jacob’, ‘Dunn’, ‘SQL101’),
+  (‘76563’, ’Dale’, ‘Cook’, ‘COMPSCI101’),
+  (‘76606’, ‘Robert’, ‘Johnson’, ‘COMPSCI101’),
+  (‘72001’, ‘Casey’, ‘Lawrence’, ‘COMPSCI101’),
+  (‘25276’, ‘Thomas’, ‘Chambers’, ‘COMPSCI101’),
+  (‘13871’, ‘Shannon’, ‘Sanchez’, ‘COMPSCI101’)
+
+
